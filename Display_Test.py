b09c30605ddb1e016a642b84fb9bea68a353bc03
jhavens <jhavens12@gmail.com>
2018-04-02 11:13:24 -0400

    subview

--- Display_Test.py
+++ Display_Test.py
@@ -4,39 +4,53 @@
 import ui
 
 w,h = ui.get_screen_size()
-h = h - 300
+h = h
 view = ui.View(bg_color = 'lightyellow', frame = (0,0,w,h))
 #view.flex = 'WH'
 # label height and button width
-bh = bw = 80
+bh = 32
+bw = 80
 # margin
-mg = 20
+
+smg = 5
+tmg = 20
+
 
 #view_1 =  ui.View([frame=(0, 0, 50, 50), flex='', background_color=black, name=None])
-view_1 =  ui.View(frame=(0, 0, 50, 50), background_color=black)
+view_1 =  ui.View(frame=(tmg, smg, w/2, h), background_color='black')
 
-lb1 = ui.Label(name = 'Label1', bg_color = 'yellow', frame =(mg,mg,bw,bh))
+bt1 = ui.Button(name = 'Button1', bg_color ='white', frame = (tmg, smg, bw, bh))
+bt1.border_color = 'black'
+bt1.tint_color = 'black'
+bt1.border_width = 1
+bt1.alignment=1
+bt1.title = 'BUTTON1'
+
+view_1.add_subview(lb1)
+
+
+lb1 = ui.Label(name = 'Label1', bg_color = 'yellow', frame =(smg,tmg,bw,bh))
 lb1.border_color = 'black'
 lb1.border_width = 1
 lb1.flex = 'RB'
 lb1.alignment=1
 lb1.text = lb1.flex
 
-lb2 = ui.Label(name = 'Label2', bg_color = 'yellow', frame =(w-(bw+mg),mg, bw,bh))
+lb2 = ui.Label(name = 'Label2', bg_color = 'yellow', frame =(w-(bw+smg),smg, bw,bh))
 lb2.border_color = 'black'
 lb2.border_width = 1
 lb2.flex = 'LB'
 lb2.alignment=1
 lb2.text = lb2.flex
 
-lb3 = ui.Label(name = 'Label3', bg_color = 'yellow', frame =(mg,h-(bh+mg),bw,bh))
+lb3 = ui.Label(name = 'Label3', bg_color = 'yellow', frame =(smg,h-(bh+smg),bw,bh))
 lb3.border_color = 'black'
 lb3.border_width = 1
 lb3.flex = 'RT'
 lb3.alignment=1
 lb3.text = lb3.flex
 
-lb4 = ui.Label(name = 'Label4', bg_color = 'yellow', frame =(w-(bw+mg),h-(bh+mg),bw,bh))
+lb4 = ui.Label(name = 'Label4', bg_color = 'yellow', frame =(w-(bw+smg),h-(bh+smg),bw,bh))
 lb4.border_color = 'black'
 lb4.border_width = 1
 lb4.flex = 'LT'
